76329305084715ee98174935f66ae2c451438a92
RAEES KHAN <inforaees6908090@gmail.com>
2025-12-20 19:02:40 +0500

    changeing

--- server.js
+++ server.js
@@ -30,5 +30,5 @@ mongoose
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ DB error:", err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
